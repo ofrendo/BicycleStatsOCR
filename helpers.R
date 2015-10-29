202f0a8adc08fd694cfdde01d4ef3da25ff66f87
Oliver <ofrendo@gmail.com>
2015-10-29 10:09:06 +0100

    Updated matrix calculations

--- helpers.R
+++ helpers.R
@@ -8,3 +8,12 @@ library(fpc)
 #library(dbscan)
 
 rotateM <- function(m) t(m)[,nrow(m):1]
+
+
+
+
+#Probably not needed
+getRed <- function(x) floor(x %% 0x1000000 / 0x10000)
+getGreen <- function(x) floor(x %% 0x10000 / 0x100)
+getBlue <- function(x) floor(x %% 0x100)
+getRGB <- function(x) c(getRed(x), getGreen(x), getBlue(x))
--- main.R
+++ main.R
@@ -5,7 +5,8 @@ source("loadData.R")
 # 0) PREPROCESSING
 # Load image in pixels
 # Convert to greyscale, leaving red ones as black and others as white
-path <- "img/IMG_20151027_224017.jpg"
+path <- "img/IMG_20151027_224017.jpg" # low res pic
+#path <- "img/IMG_20151028_133125.jpg" # day time pic
 # bright pixel
 #x <- 1737
 #y <- 1373
@@ -38,55 +39,69 @@ getBinary <- function() {
 img.binary <- getBinary()
 
 # plot channels
-#plot(stack(img.red, img.green, img.blue, binary.img), main = c("red", "green", "blue", "binary"))
+#plot(stack(img.red, img.green, img.blue, img.binary), main = c("red", "green", "blue", "binary"))
 plot(stack(img.binary))  
 
 
 # 1) TOKENIZATION
 # With DBSCAN
 getDBScan <- function(eps, MinPts) {
-  # transform matrix of 
-  # 0 0 1
-  # 0 1 0
-  # to points
-  # 1 3
-  # 2 2
   binary <- rotateM(as.matrix(img.binary))
-  binaryPoints <- matrix(nrow=NROW(binary[binary==1]), ncol=2)
-  counter <- 0
-  for (i in 1:NROW(binary)) {
-    for (j in 1:NCOL(binary)) {
-      if (binary[i, j] == 1) {
-        counter <- counter + 1
-        binaryPoints[counter,] = c(i, j)
-      }
-    }
-  }
-  
-  #binaryPoints <- 
-  #binary <- as.vector(img.binary)
+  binaryPoints <<- toPointsMatrix(binary)
+
   result <- dbscan(binaryPoints, eps=eps, MinPts=MinPts, method="raw")
 }
 
 img.dbscan <- getDBScan(eps = 2, MinPts = 11)
 img.clusters <- img.dbscan$cluster
 plot(img.dbscan, binaryPoints)
-plot(binaryPoints[img.clusters==1,])
+plot(binaryPoints[img.clusters==4,])
+
+singleDigit <- binaryPoints[img.clusters==1,]
+singleDigitPixels <- toPixelMatrix(singleDigit)
 
 
 
 
+# transform matrix of 
+# 0 0 1
+# 0 1 0
+# to points
+# 1 3
+# 2 2
+toPointsMatrix <- function(m) {
+  result <- matrix(nrow=NROW(m[m==1]), ncol=2)
+  counter <- 0
+  for (i in 1:NROW(m)) {
+    for (j in 1:NCOL(m)) {
+      if (m[i, j] == 1) {
+        counter <- counter + 1
+        result[counter,] <- c(i, j)
+      }
+    }
+  }
+  result
+}
+
+toPixelMatrix <- function(m) {
+  result <- matrix(nrow=max(m[,2])-min(m[,2]), ncol=max(m[,1])-min(m[,1]))
+  
+  minX <- min(m[,1])
+  maxX <- max(m[,1])
+  dx <- maxX-minX
+  minY <- min(m[,2])
+  result[] <- 0
+  for (i in 1:NROW(m)) {
+    row <- m[i,2]-minY
+    col <- m[i,1]-minX
+    #print(row)
+    #print(col)
+    
+    result[row,col] <- 1
+  }
+  result <- result[,NCOL(result):1]
+  result
+}
 
 
 
-
-
-
-
-
-
-#Probably not needed
-getRed <- function(x) floor(x %% 0x1000000 / 0x10000)
-getGreen <- function(x) floor(x %% 0x10000 / 0x100)
-getBlue <- function(x) floor(x %% 0x100)
-getRGB <- function(x) c(getRed(x), getGreen(x), getBlue(x))
